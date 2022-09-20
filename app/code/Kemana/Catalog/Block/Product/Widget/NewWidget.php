93c7d9fd88c11472488eac7a68cfb161ca21e017
parth <pgodhani@kemana.com>
2022-09-20 15:11:18 +0530

    home-page-changes

--- app/code/Kemana/Catalog/Block/Product/Widget/NewWidget.php
+++ app/code/Kemana/Catalog/Block/Product/Widget/NewWidget.php
@@ -10,77 +10,8 @@ namespace Kemana\Catalog\Block\Product\Widget;
  */
 class NewWidget extends \Magento\Catalog\Block\Product\NewProduct
 {
-        /**
-     * Display products type - all products
-     */
-    const DISPLAY_TYPE_ALL_PRODUCTS = 'all_products';
 
     const TITILE = 'New Products';
-
-    /**
-     * Display products type - new products
-     */
-    const DISPLAY_TYPE_NEW_PRODUCTS = 'new_products';
-
-    /**
-     * Default value whether show pager or not
-     */
-    const DEFAULT_SHOW_PAGER = false;
-
-    /**
-     * Default value for products per page
-     */
-    const DEFAULT_PRODUCTS_PER_PAGE = 5;
-
-    /**
-     * Name of request parameter for page number value
-     *
-     * @deprecated
-     */
-    const PAGE_VAR_NAME = 'np';
-
-    /**
-     * Instance of pager block
-     *
-     * @var \Magento\Catalog\Block\Product\Widget\Html\Pager
-     */
-    protected $_pager;
-
-    /**
-     * @var \Magento\Framework\Serialize\Serializer\Json
-     */
-    private $serializer;
-
-    /**
-     * NewWidget constructor.
-     *
-     * @param \Magento\Catalog\Block\Product\Context $context
-     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
-     * @param \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility
-     * @param \Magento\Framework\App\Http\Context $httpContext
-     * @param array $data
-     * @param \Magento\Framework\Serialize\Serializer\Json|null $serializer
-     */
-    public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
-        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
-        \Magento\Framework\App\Http\Context $httpContext,
-        array $data = [],
-        \Magento\Framework\Serialize\Serializer\Json $serializer = null
-    ) {
-        parent::__construct(
-            $context,
-            $productCollectionFactory,
-            $catalogProductVisibility,
-            $httpContext,
-            $data,
-            $serializer
-        );
-        $this->serializer = $serializer ?: \Magento\Framework\App\ObjectManager::getInstance()
-            ->get(\Magento\Framework\Serialize\Serializer\Json::class);
-    }
-
 
     public function getTitle()
     {
