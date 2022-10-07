d7521dd3ae5be16568c18fd0208cf0c5f4461422
Parth <pgodhani@kemana.com>
2022-10-07 12:24:09 +0530

    msdynamic-enable-disable-functionality

--- app/code/Kemana/MsDynamics/view/frontend/web/js/update-stock.js
+++ app/code/Kemana/MsDynamics/view/frontend/web/js/update-stock.js
@@ -56,22 +56,27 @@ define([
                   id: $widget.options.productid
                 },
                 success: function(response) {
-                    if(response){
-                        if($widget.options.producttype == 'simple'){
-                           if(response.response.Inventory > 0){
-                                $(".product-add-form").show();
-                                $(".product.alert.stock").hide();
-                                $(".product-info-stock-sku .stock").addClass("available").html("<span>In stock</span>")
-                           }else{
-                                $(".product-add-form").hide();
-                                $(".product.alert.stock").show();
-                                $(".product-info-stock-sku .stock").addClass("unavailable").html("<span>Out of stock</span>")
-                           }
+                    if(response.msDynamics){
+                        if(response.apiresponse){
+                            if($widget.options.producttype == 'simple'){
+                               if(response.apiresponse.Inventory > 0){
+                                    $(".product-add-form").show();
+                                    $(".product.alert.stock").hide();
+                                    $(".product-info-stock-sku .stock").addClass("available").html("<span>In stock</span>")
+                               }else{
+                                    $(".product-add-form").hide();
+                                    $(".product.alert.stock").show();
+                                    $(".product-info-stock-sku .stock").addClass("unavailable").html("<span>Out of stock</span>")
+                               }
+                            }
+                        }else{
+                            $(".product-add-form").hide();
+                            $(".product.alert.stock").show();
+                            $(".product-info-stock-sku .stock").addClass("unavailable").html("<span>Out of stock</span>")
                         }
                     }else{
-                        $(".product-add-form").hide();
+                        $(".product-add-form").show();
                         $(".product.alert.stock").show();
-                        $(".product-info-stock-sku .stock").addClass("unavailable").html("<span>Out of stock</span>")
                     }
                 }
             });
