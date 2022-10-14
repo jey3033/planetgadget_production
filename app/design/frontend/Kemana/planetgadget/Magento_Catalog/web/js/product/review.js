b23f30e3b780bf3d9467d7dbc511fde617978363
parth <pgodhani@kemana.com>
2022-10-14 16:23:43 +0530

    bugfixing-114

---
+++ app/design/frontend/Kemana/planetgadget/Magento_Catalog/web/js/product/review.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright © 2022 PT Kemana Teknologi Solusi. All rights reserved.
+ * http://www.kemana.com
+ */
+
+/**
+ * @category Kemana
+ * @package  Kemana_MsDynamics
+ * @license  http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @author   Parth Godhani <pgodhani@kemana.com>
+ */
+
+
+define([
+    'Magento_Ui/js/grid/columns/column',
+    'Magento_Catalog/js/product/list/column-status-validator'
+], function (Column, columnStatusValidator) {
+    'use strict';
+
+    return Column.extend({
+
+        /**
+         * @param row
+         * @returns {boolean}
+         */
+        hasValue: function (row) {
+            return "review" in row['extension_attributes'];
+        },
+
+        /**
+         * @param row
+         * @returns {*}
+         */
+        getValue: function (row) {
+            return row['extension_attributes']['review'] ? row['extension_attributes']['review']+"%" : '';
+        },
+
+        /**
+         * @param row
+         * @returns {*|boolean}
+         */
+        isAllowed: function (row) {
+            return (columnStatusValidator.isValid(this.source(), 'review', 'show_attributes') && this.hasValue(row) );
+        }
+
+    });
+});
