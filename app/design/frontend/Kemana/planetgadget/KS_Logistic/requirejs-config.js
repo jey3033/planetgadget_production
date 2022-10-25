19df10e4a8ad3e6acf3b55c8fd2db2b195be5ac7
Nipun Nirmana <nnirmana@kemana.com>
2022-10-25 12:39:22 +0530

    PG-344 - Adding country_id as a shipping validator rule for JNE

---
+++ app/design/frontend/Kemana/planetgadget/KS_Logistic/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'KS_Logistic/js/model/shipping-rates-validation-rules/jne': {
+                'KS_Logistic/js/model/shipping-rates-validation-rules/jne-mixin': true
+            }
+        }
+    }
+};
